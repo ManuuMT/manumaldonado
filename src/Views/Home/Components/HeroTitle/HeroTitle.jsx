32b4863a24f4a9d827b467ee2abeb9e1238371f1
ManuuMT <ema14mt@gmail.com>
2022-05-07 01:55:10 +0200

    Background changed to a GIF

--- src/Views/Home/Components/HeroTitle/HeroTitle.jsx
+++ src/Views/Home/Components/HeroTitle/HeroTitle.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-import imageSource from "../../../../assets/img/background.jpg";
-import "./HeroTitle.css";
 import profileSource from "../../../../assets/img/profile-rounded.png";
+import "./HeroTitle.css";
+
 
 const HeroTitle = () => {
     return(
         <main className="main" >
             <div className="row main-title">
-                <div className="col-3">
-                
-                </div>
+                <div className="col-lg-3 col-md-12"></div>
 
-                <div className="col-3">
-                    <img src={profileSource} className="main-pic"/>
-                </div>
-                <div className="col-4">
-                    <div className="main-message text-center">
-                            <h3>Hello!</h3>
-                            <h1>I'm Manu Maldonado</h1>
-                            <h2>- Full Stack Developer -</h2>
+                <div className="col-lg-6 col-md-10">
+                    <div className="row content">
+                        <div className="col-lg-4 col-md-12">
+                            <img src={profileSource} className="main-pic"/>
+                        </div>
+                        <div className="col-lg-8 col-md-12">
+                            <div className="main-message text-center">
+                                <h3>Hello!</h3>
+                                <h1>I am Emanuel</h1>
+                                <h2>- Full Stack Developer -</h2>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="col-2">
-                
-                </div>
+                   
+                </div>      
+                <div className="col-lg-3 col-md-2"></div>
             </div>
-            <img src={imageSource} className="background-img"/>
+            <img src="https://i.imgur.com/4oHOotx.gif" className="background-img" alt="Loading..." />
+            {/*<img src={imageSource} className="background-img"/>*/}
         </main>
     );
 }
