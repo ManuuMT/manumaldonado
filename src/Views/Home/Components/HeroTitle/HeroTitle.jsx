7f5c622de9c91cdbed1c2a365c11194f6968a8d0
ManuuMT <ema14mt@gmail.com>
2022-05-07 10:38:59 +0200

    Title style fix

--- src/Views/Home/Components/HeroTitle/HeroTitle.jsx
+++ src/Views/Home/Components/HeroTitle/HeroTitle.jsx
@@ -11,14 +11,14 @@ const HeroTitle = () => {
 
                 <div className="col-md-8 col-lg-6">
                     <div className="row d-flex align-items-center">
-                        <div className="col-md-6 col-sm-12 text-center">
+                        <div className="col-md-4 col-sm-12 text-center">
                             <img src={profileSource} alt="manu-maldonado" className="main-pic"/>
                         </div>
-                        <div className="col-md-6 col-sm-12 py-5">
+                        <div className="col-md-8 col-sm-12 py-5">
                             <div className="main-message text-center">
                                 <h3>Hello!</h3>
                                 <h1 className="name"><strong>I am Emanuel</strong></h1>
-                                <h2>- Full Stack Developer -</h2>
+                                <h2 className="title">- Full Stack Developer -</h2>
                             </div>
                         </div>
                     </div> 
