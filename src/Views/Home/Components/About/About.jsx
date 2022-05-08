e395b4b9f8bc80f32c72609dc308731a478cd9ec
ManuuMT <ema14mt@gmail.com>
2022-05-09 00:33:52 +0200

    Icons and sources added

--- src/Views/Home/Components/About/About.jsx
+++ src/Views/Home/Components/About/About.jsx
@@ -1,17 +1,45 @@
 import React from "react";
-import "./About.css";
-import sass from "../../../../assets/img/tech/sass.png";
+import "./About.scss";
+import Tech from "./Tech.jsx";
+import { Sources } from "./Sources.js";
 
 const About = () => {
     return(
     
-    <div className="container about">
-        <div className="card d-flex justify-content-center align-items-center">
-            <div className="text">
-                <h3>SKILLS</h3>
+    <div className="container-fluid">
+        <div className="row mt-5">
+            <div className="col-4">
+                <div className="text d-flex justify-content-center align-items-center">
+                    <h3>ABOUT ME</h3>
+                </div>
             </div>
-        </div>
-        <img src={sass} className="skill" alt="sass"></img>
+
+            <div className="col-8">
+                    <div className="row">
+                        <div className="col"></div>
+                        <div className="col">
+                            <div className="text d-flex justify-content-center align-items-center ">
+                                <h3>SKILLS</h3>
+                            </div>
+                        </div>
+                        <div className="col"></div>
+                    </div>
+                    
+            
+                    <div className="menu"> 
+                        <div className="row">
+                                    <Tech title="HTML5" src={Sources.html5}/>   
+                                    <Tech title="CSS3" src={Sources.css}/>
+                                    <Tech title="Javascript" src={Sources.javascript}/>
+                                    <Tech title="HTML5" src={Sources.html5}/>   
+                                    <Tech title="CSS3" src={Sources.css}/>
+                                    <Tech title="Javascript" src={Sources.javascript}/>
+                        </div> 
+                        
+                    </div>     
+                
+            </div>
+        </div>        
     </div>
     
         
---
+++ src/Views/Home/Components/About/Sources.js
@@ -0,0 +1,27 @@
+import bootstrap from "../../../../assets/img/tech/bootstrap.png";
+import css from "../../../../assets/img/tech/css.png";
+import flask from "../../../../assets/img/tech/flask.png";
+import git from "../../../../assets/img/tech/git.png";
+import html5 from "../../../../assets/img/tech/html5.png";
+import javascript from "../../../../assets/img/tech/javascript.png";
+import jest from "../../../../assets/img/tech/jest.png";
+import python from "../../../../assets/img/tech/python.png";
+import react from "../../../../assets/img/tech/react.png";
+import sass from "../../../../assets/img/tech/sass.png";
+import sql from "../../../../assets/img/tech/sql.png";
+import typescript from "../../../../assets/img/tech/typescript.png";
+
+export const Sources = {
+    bootstrap: bootstrap,
+    css: css,
+    flask: flask,
+    git: git,
+    html5: html5,
+    javascript: javascript,
+    jest: jest,
+    python: python,
+    react: react,
+    sass: sass,
+    sql: sql,
+    typescript: typescript 
+}
