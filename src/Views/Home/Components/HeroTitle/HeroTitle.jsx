a669b0656e31993ba3ffcc723b6d5b17f5d0b158
ManuuMT <ema14mt@gmail.com>
2022-05-09 08:20:29 +0200

    Navbar bug fixed

--- src/Views/Home/Components/HeroTitle/HeroTitle.jsx
+++ src/Views/Home/Components/HeroTitle/HeroTitle.jsx
@@ -1,34 +1,41 @@
 import React from "react";
 import profileSource from "../../../../assets/img/profile-rounded.png";
-import "./HeroTitle.css";
+import "./HeroTitle.scss";
 //  d-flex justify-content-center align-items-center
 
 const HeroTitle = () => {
-    return(<>
-        <div className="container-fluid main" >
-            <div className="row main-row">
-                <div className="col-md-2 col-lg-3"></div>
+  return (
+    <>
+      <div className="container-fluid main">
+        <div className="row main-row">
+          <div className="col-md-2 col-lg-3"></div>
 
-                <div className="col-md-8 col-lg-6">
-                    <div className="row d-flex align-items-center">
-                        <div className="col-md-4 col-sm-12 text-center">
-                            <img src={profileSource} alt="manu-maldonado" className="main-pic"/>
-                        </div>
-                        <div className="col-md-8 col-sm-12 py-5">
-                            <div className="main-message text-center">
-                                <h3>Hello!</h3>
-                                <h1 className="name"><strong>I am Emanuel</strong></h1>
-                                <h2 className="title">- Full Stack Developer -</h2>
-                            </div>
-                        </div>
-                    </div> 
-                </div>     
-
-                <div className="col-md-2 col-lg-3"></div>
+          <div className="col-md-8 col-lg-6">
+            <div className="row d-flex align-items-center">
+              <div className="col-md-4 col-sm-12 text-center">
+                <img
+                  src={profileSource}
+                  alt="manu-maldonado"
+                  className="main-pic"
+                />
+              </div>
+              <div className="col-md-8 col-sm-12 py-5">
+                <div className="main-message text-center">
+                  <h3>Hello!</h3>
+                  <h1 className="name">
+                    <strong>I am Emanuel</strong>
+                  </h1>
+                  <h2 className="title">- Full Stack Developer -</h2>
+                </div>
+              </div>
             </div>
+          </div>
+
+          <div className="col-md-2 col-lg-3"></div>
         </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default HeroTitle;
--- src/Views/NavBar/NavBar.jsx
+++ src/Views/NavBar/NavBar.jsx
@@ -1,58 +1,54 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./NavBar.css";
+import "./NavBar.scss";
 
 const NavBar = () => {
-    const [state, setState] = useState("off");
-    const title = "< Manu />";
+  const [state, setState] = useState("off");
+  const title = "< Manu />";
 
-    const HandleClick = () => {
-        state==="off" ? setState("") : setState("off");
-      };
+  const HandleClick = () => {
+    state === "off" ? setState("") : setState("off");
+  };
 
-    return(
-        <header className="header">
-            <nav className="nav">
-                
-                <Link to="/">
-                    <h3 className="logo">{title}</h3>
-                </Link>
+  return (
+    <header className="header">
+      <nav className="nav">
+        <Link to="/">
+          <h3 className="logo">{title}</h3>
+        </Link>
+        <button className="nav-toggle" onClick={() => HandleClick()}>
+          <i className="fas fa-bars"></i>
+        </button>
+        <div className={state}>
+          <ul className="nav-menu">
+            <li className="nav-menu-item">
+              <Link to="/">
+                <div className="navbar-link">Home</div>
+              </Link>
+            </li>
 
-                <button className="nav-toggle" onClick={() => HandleClick()}>
-                    <i className="fas fa-bars"></i>
-                </button>
+            <li className="nav-menu-item">
+              <Link to="/about">
+                <div className="navbar-link">About</div>
+              </Link>
+            </li>
 
-                <div className={state}>
-                    <ul className="nav-menu">
-                        
-                        <li className="nav-menu-item">  
-                            <Link to="/">
-                                <div className="navbar-link">Home</div>
-                            </Link>
-                        </li>
+            <li className="nav-menu-item">
+              <Link to="/contact">
+                <div className="navbar-link">Contact</div>
+              </Link>
+            </li>
 
-                        <li className="nav-menu-item">
-                            <Link to="/about">
-                                <div className="navbar-link">About</div>
-                            </Link>
-                        </li>
-
-                        <li className="nav-menu-item">
-                            <Link to="/contact">
-                                <div className="navbar-link">Contact</div>
-                            </Link>
-                        </li>
-
-                        <li className="nav-menu-item">
-                            <Link to="/idontknow">
-                                <button className="navbar-link nav-button">Enviar</button>
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav> 
-        </header>
-    );
-}
+            <li className="nav-menu-item">
+              <Link to="/idontknow">
+                <button className="navbar-link nav-button">Enviar</button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
