949e7ad84492bea8cfc0c0b192403e52d3d0f5e6
ManuuMT <ema14mt@gmail.com>
2022-05-11 20:32:00 +0200

    About section refactorized

--- src/Views/Home/Components/About/About.jsx
+++ src/Views/Home/Components/About/About.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./About.scss";
-import linkedin from "../../../../assets/img/social/linkedin.png";
-import github from "../../../../assets/img/social/github.png";
-import mail from "../../../../assets/img/social/mail.png";
+import linkedinIcon from "../../../../assets/img/social/linkedin.png";
+import githubIcon from "../../../../assets/img/social/github.png";
+import mailIcon from "../../../../assets/img/social/mail.png";
+
+const linkedin = "https://www.linkedin.com/in/emanuel-mt";
+const github = "https://www.github.com/ManuuMT";
+const mail = "mailto:emtuero@hotmail.com";
 
 const About = () => {
   return (
-    <div className="container-fluid about-body pt-5" id="about">
-      <div className="row text-center">
-        <div className="col-lg-3 col-md-2 col-sm-0"></div>
-        <div className="col-lg-6 col-md-8 col-sm-12">
-          <div className="row">
+    <div
+      className="container-fluid about-body d-flex align-items-center justify-content-center"
+      id="about"
+    >
+      <div className="row">
+        <div className="col-md-6 col-sm-12">
+          <div className="row text-center">
             <h3 className="name">About Me</h3>
           </div>
           <div className="row mt-2">
-            <p className="body-content">
+            <p className="body-content text-center">
               My name is Emanuel Maldonado Tuero, but everyone calls me
               <b> Manu</b>.
               <br /> I am 26 years old. I was born in <b>Argentina</b> and I've
@@ -23,34 +29,30 @@ const About = () => {
               my job and change my life, making programming my full time job.
             </p>
           </div>
-          <div className="row mt-4">
+        </div>
+        <div className="col-md-6 col-sm-12">
+          <div className="row contact-row text-center">
+            <h3 className="name">Contact Me</h3>
+          </div>
+          <div className="row mt-2">
             <div className="social-media">
-              <a
-                href="https://www.linkedin.com/in/emanuel-mt"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <img className="social-icon" src={linkedin} alt="linkedin" />
+              <a href={linkedin} rel="noreferrer" target="_blank">
+                <img
+                  className="social-icon"
+                  src={linkedinIcon}
+                  alt="linkedin"
+                />
               </a>
-              <a
-                href="https://www.github.com/ManuuMT"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <img className="social-icon" src={github} alt="github" />
+              <a href={github} rel="noreferrer" target="_blank">
+                <img className="social-icon" src={githubIcon} alt="github" />
               </a>
-              <a
-                href="mailto:emtuero@hotmail.com"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <img className="social-icon" src={mail} alt="mail" />
+              <a href={mail} rel="noreferrer" target="_blank">
+                <img className="social-icon" src={mailIcon} alt="mail" />
               </a>
             </div>
           </div>
         </div>
       </div>
-      <div className="col-lg-3 col-md-2 col-sm-0"></div>
     </div>
   );
 };
