f5164c715660eafcb968422262bf5c039f012922
Emanuel Maldonado <ema14mt@gmail.com>
2022-11-28 06:37:16 -0300

    About me refactor

--- src/Views/Home/Components/About/About.jsx
+++ src/Views/Home/Components/About/About.jsx
@@ -10,47 +10,32 @@ const mail = "mailto:emtuero@hotmail.com";
 
 const About = () => {
   return (
-    <div
-      className="container-fluid about-body d-flex align-items-center justify-content-center"
-      id="about"
-    >
-      <div className="row">
-        <div className="col-md-6 col-sm-12">
-          <div className="row text-center">
-            <h3 className="name">About Me</h3>
-          </div>
-          <div className="row mt-2">
-            <p className="body-content text-center">
-              My name is Emanuel Maldonado Tuero, but everyone calls me
-              <b> Manu</b>.
-              <br /> I am 26 years old. I was born in <b>Argentina</b> and I've
-              been living in <b>Spain</b> since 2018. <br />I spent the last 11
-              years working in customer service, but in 2021 I decided to quit
-              my job and change my life, making programming my full time job.
-            </p>
-          </div>
+    <div className="about" id="about">
+      <div className="about-description">
+        <h2 className="about-title">About Me</h2>
+        <div className="about-description-text">
+          <p>
+            My name is Emanuel Maldonado Tuero, but everyone calls me
+            <strong> Manu</strong>. I am 27 years old. I was born in
+            <strong> Argentina</strong> and I've been living in
+            <strong> Spain</strong> since 2018. I spent the last 11 years
+            working in customer service, but in 2021 I decided to quit my job
+            and change my life, making programming my full time job.
+          </p>
         </div>
-        <div className="col-md-6 col-sm-12">
-          <div className="row contact-row text-center">
-            <h3 className="name">Contact Me</h3>
-          </div>
-          <div className="row mt-2">
-            <div className="social-media">
-              <a href={linkedin} rel="noreferrer" target="_blank">
-                <img
-                  className="social-icon"
-                  src={linkedinIcon}
-                  alt="linkedin"
-                />
-              </a>
-              <a href={github} rel="noreferrer" target="_blank">
-                <img className="social-icon" src={githubIcon} alt="github" />
-              </a>
-              <a href={mail} rel="noreferrer" target="_blank">
-                <img className="social-icon" src={mailIcon} alt="mail" />
-              </a>
-            </div>
-          </div>
+      </div>
+      <div className="about-contact">
+        <h2 className="about-title">Contact Me</h2>
+        <div className="social-media">
+          <a href={linkedin} rel="noreferrer" target="_blank">
+            <img className="social-icon" src={linkedinIcon} alt="linkedin" />
+          </a>
+          <a href={github} rel="noreferrer" target="_blank">
+            <img className="social-icon" src={githubIcon} alt="github" />
+          </a>
+          <a href={mail} rel="noreferrer" target="_blank">
+            <img className="social-icon" src={mailIcon} alt="mail" />
+          </a>
         </div>
       </div>
     </div>
