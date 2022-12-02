26481ee88c4106e74f219475b07cf26caa133535
Emanuel Maldonado <ema14mt@gmail.com>
2022-12-02 04:10:22 -0300

    Intro screen created

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar.jsx";
 import Skills from "./Components/Skills/Skills.jsx";
 import HeroTitle from "./Components/HeroTitle/HeroTitle.jsx";
 import About from "./Components/About/About.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
+import "./Home.scss";
+const title = "<ManuDev />";
 
 const Home = () => {
+  // * States
+  const [loading, setLoading] = useState(true);
+
+  // * Methods
+  const Intro = () => (
+    <div className="intro">
+      <div className="intro-left-window" />
+      <h1 className="intro-logo">{title}</h1>
+      <div className="intro-right-window" />
+    </div>
+  );
+
+  // * Life Cycle
+  useEffect(() => {
+    document.addEventListener("animationend", (event) => {
+      if (event.animationName === "closeToRight") setLoading(false);
+    });
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <HeroTitle />
-      <Skills />
-      <About />
-      <Footer />
+      {loading ? (
+        Intro()
+      ) : (
+        <>
+          <Navbar />
+          <HeroTitle />
+          <Skills />
+          <About />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
