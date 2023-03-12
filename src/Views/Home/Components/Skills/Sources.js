077beae7b3bb44e4f7867ce88e0340aad03bfbe7
Emanuel Maldonado <ema14mt@gmail.com>
2023-03-12 04:23:54 -0300

    New tech added

--- src/Views/Home/Components/Skills/Sources.js
+++ src/Views/Home/Components/Skills/Sources.js
@@ -8,8 +8,17 @@ import jest from "../../../../assets/img/tech/jest.png";
 import python from "../../../../assets/img/tech/python.png";
 import react from "../../../../assets/img/tech/react.png";
 import sass from "../../../../assets/img/tech/sass.png";
-import sql from "../../../../assets/img/tech/sql.png";
 import typescript from "../../../../assets/img/tech/typescript.png";
+import mongo from "../../../../assets/img/tech/mongo.png";
+import mongoose from "../../../../assets/img/tech/mongoose.png";
+import node from "../../../../assets/img/tech/node.png";
+import mysql from "../../../../assets/img/tech/mysql.png";
+import sequelize from "../../../../assets/img/tech/sequelize.png";
+import redux from "../../../../assets/img/tech/redux.png";
+import tailwind from "../../../../assets/img/tech/tailwind.png";
+import material from "../../../../assets/img/tech/material.png";
+import reacttesting from "../../../../assets/img/tech/react-testing.png";
+import express from "../../../../assets/img/tech/express.png";
 import adobePS from "../../../../assets/img/tools/adobe-ps.png";
 import adobeXD from "../../../../assets/img/tools/adobe-xd.png";
 import figma from "../../../../assets/img/tools/figma.png";
@@ -21,15 +30,24 @@ export const SkillSet = [
   { src: html5, title: "HTML5" },
   { src: css, title: "CSS3" },
   { src: javascript, title: "Javascript" },
+  { src: typescript, title: "Typescript" },
   { src: react, title: "React" },
-  { src: bootstrap, title: "Bootstrap" },
+  { src: redux, title: "Redux" },
+  { src: node, title: "NodeJS" },
+  { src: express, title: "Express" },
+  { src: mongo, title: "MongoDB" },
+  { src: mongoose, title: "Mongoose" },
+  { src: mysql, title: "MySQL" },
+  { src: sequelize, title: "Sequelize" },
   { src: sass, title: "SASS" },
+  { src: tailwind, title: "Tailwind CSS" },
+  { src: material, title: "Material UI" },
+  { src: bootstrap, title: "Bootstrap" },
   { src: python, title: "Python" },
   { src: flask, title: "Flask" },
-  { src: typescript, title: "Typescript" },
   { src: git, title: "Git" },
-  { src: sql, title: "SQL" },
   { src: jest, title: "Jest" },
+  { src: reacttesting, title: "React Testing Library" },
 ];
 
 export const Tools = [
